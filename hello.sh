2123bdce8c1e847440b957532c9ca29c13c9a34e
Leon Dawert <leon@ldawert.de>
2022-04-13 10:07:41 +0200

    add git-go

--- hello.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ME=$(whoami)
-TODAY=$(date '+%A, %B %d')
-echo ""
-echo "Hello $ME! Today is $TODAY"
-echo ""
-curl wttr.in/kiel?0
--- motd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ME=$(whoami)
-/usr/local/bin/artii "Welcome   $ME!" | /usr/local/bin/lolcat
-#/usr/local/bin/fortune | /usr/local/bin/cowsay | /usr/local/bin/lolcat
